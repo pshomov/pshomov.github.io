cdf6e65b18b168f986ab620628cf6f5aeefcbc78
Petar Shomov <petar@activitystream.com>
2015-07-12 02:29:48 +0300

    cleaning up build artifacts from before

--- update_site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-read -p "Continue (y/n)?" choice
-case "$choice" in 
-  y|Y ) git subtree push --prefix _site origin master ;;
-  n|N ) echo "That was close ...";;
-  * ) echo "invalid";;
-esac
-
